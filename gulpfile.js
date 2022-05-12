b11ff4054b0e478fb326840e43d5faf36f1d0932
Kassem <systemry420@gmail.com>
2022-05-12 20:52:02 +0300

    playing with math, map and color shades(loop)

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('sass/**/index.scss')
+    return src('sass/**/*.scss')
         .pipe(sass())
         .pipe(dest('styles'))
 }
 
 function watchTask() {
-    watch(['sass/**/index.scss'], buildStyles)
+    watch(['sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
